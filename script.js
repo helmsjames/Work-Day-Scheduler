c1b4ff1e967e3584ac7acb9a513206e1ba29a19c
James Helms <helms.james@gmail.com>
2020-03-23 00:45:34 -0500

    got each column to render a different color, added the black vertical border, added the white horizontal border

--- script.js
+++ script.js
@@ -1,77 +1,52 @@
-// const clock = getElementById("currentDay");
-// var now = moment();
-// setInterval(() => {
-//     const now = moment();
-//     console.log(moment());
-// const humanReadable = now.format('dddd');
-// console.log(humanReadable);
-//   }, 1000);
-// var timeInc = 9;
-//     var timeOfDay = " AM";
-//     for (var i = 0; i < 9; i++){
-//       $(".row").append(dateRow)
-//       if(timeInc < 12){
-//         var previousText = localStorage.getItem(timeInc)
-//         var dateRow = $('<div class="col-sm-2">'+ timeInc + timeOfDay +'</div><div class="col-sm-8"><input type="text" class='+ timeInc + ' > ' + previousText + '</div><div class="col-sm-2"><button class="btn btn-primary" data-name='+ timeInc + '>*</button></div>')
-//         timeInc++;
-//       }
-//       else{
-//         timeOfDay = " PM"
-//         timeInc = 1;
-//         timeInc++;
-//       }
 
-// import { create } from "domain";
-
-// A $( document ).ready() block.
-$( document ).ready(function() {
+$(document).ready(function () {
   const times = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
   const container = $(".container");
   times.forEach(time => {
-  container.append(timeSlot(time));
+    container.append(timeSlot(time));
   });
-  console.log( "ready!" );
-  $(".button").click(function() {
+  console.log("ready!");
+  $(".button").click(function () {
     var momentObj = moment();
     var newDate = momentObj.format("dddd, MMMM Do YYYY");
     $("#day").text(newDate);
 
   });
-  
+
 });
 
 function timeSlot(time) {
-  let appointmentData = localStorage.getItem(time)||"";
+  let appointmentData = localStorage.getItem(time) || "";
   const slot = $(`<div class="row ${getTimeClass(time)}">
 <div class="col-sm-1 time-block">${time}</div>
-<div class="col-sm-10"><textarea id="${time}-data">${appointmentData}
+<div class="col-sm-10 text-area"><textarea id="${time}-data">${appointmentData}
 </textarea></div>
 </div>`)
-const save = $('<div class="col-sm-1 saveBtn"></div>');
-const saveButton = $(`<i class="fas fa-save" id="${time}-button"></i>`);
-  
-saveButton.click(update);
-save.append(saveButton);
-slot.append(save);
+  const save = $('<div class="col-sm-1 saveBtn"></div>');
+  const saveButton = $(`<i class="fas fa-save fa-2x" id="${time}-button"></i>`);
+
+  saveButton.click(update);
+  save.append(saveButton);
+  slot.append(save);
   return slot;
 }
-    
+
 function getTimeClass(time) {
   var lastHour = moment().hour();
   let hour = parseInt(time);
-  if(time.includes('pm') && hour != 12) {
-	hour += 12;
-}
+  if (time.includes('pm') && hour != 12) {
+    hour += 12;
+  }
 
-if(hour < lastHour) {
-  return "past";
-}
-else if (hour > lastHour) {
-  return "future";
-}
-else if(hour === lastHour) {
-  return "present";
-}
+  if (hour < lastHour) {
+    return "past";
+  }
+  else if (hour > lastHour) {
+    return "future";
+  }
+  else if (hour === lastHour) {
+    return "present";
+  }
 }
 
 function update(event) {
@@ -86,5 +61,4 @@ function update(event) {
 
 
 
-      
-      
+
